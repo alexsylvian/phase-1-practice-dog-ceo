a98afe8d0d30df162ec087b933f55ad44c09bc8f
alexsylvian <lazeratus1@gmail.com>
2023-07-25 20:32:13 -0400

    stopping here for now, still need to do the filter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+const container = document.getElementById('dog-image-container')
+const dogList = document.getElementById("dog-breeds")
+const dropdown = document.getElementById("breed-dropdown")
+
+document.addEventListener('DOMContentLoaded', () => {
+    addDogImage()
+    addDogList()
+})
+
+function addDogImage() {
+    fetch(imgUrl)
+    .then(res => res.json())
+    .then(data => {
+        data.message.forEach(pic => {
+            const doggyPic = document.createElement("img")
+            doggyPic.src = pic
+            container.appendChild(doggyPic)
+        })
+    })
+}
+
+function addDogList() {
+    fetch(breedUrl)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        const dogBreeds = Object.keys(data.message)
+        dogBreeds.forEach(entry => {
+            const breed = document.createElement("li")
+            breed.textContent = entry
+            dogList.appendChild(breed)
+            breed.addEventListener('click', () => {
+                breed.style.color = ('orange')
+            })
+        })
+    })
+}
+
+dropdown.addEventListener('change', filterBreeds)
+
+function filterBreeds() {
+    const selectedValue = dropdown.value
+    if (selectedValue === "b") {
+        console.log('b')
+    }
+}
